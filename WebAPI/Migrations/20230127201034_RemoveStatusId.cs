3a4394fda111593d8aeeb104c85c95e4509350d1
Ivan Franken <ivan.franken89@gmail.com>
2023-01-27 21:11:08 +0100

    New migration

---
+++ WebAPI/Migrations/20230127201034_RemoveStatusId.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveStatusId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StatusId",
+                table: "Onderwijsmodules");
+
+            migrationBuilder.DropColumn(
+                name: "StatusId",
+                table: "Beoordelingsmodellen");
+
+            migrationBuilder.DropColumn(
+                name: "StatusId",
+                table: "Beoordelingen");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "StatusId",
+                table: "Onderwijsmodules",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StatusId",
+                table: "Beoordelingsmodellen",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StatusId",
+                table: "Beoordelingen",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
